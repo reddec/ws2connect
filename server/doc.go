e553cfbdadd5652eab4d50c8664ccd4c2a34065d
Alexander Baryshnikov <dev@baryshnikov.net>
2019-12-24 19:44:57 +0800

    doc: extend package doc

--- server/doc.go
+++ server/doc.go
@@ -1,2 +1,7 @@
-// Reverse proxy to expose streaming service (via TCP, UDP or UNIX connection) as WebSocket
+// Reverse proxy to expose streaming service.
+//
+// Expose service via TCP, UDP or UNIX connection as WebSocket so clients can use standard protocol to communicate with
+// internal backend.
+//
+// The project was heavily inspired from socat and websocketd.
 package server
